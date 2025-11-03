c2b161e664c3d15c91f8094a11f49dbd052b2aac
Lesi Huang <Lesi.Huang@aristocrat.com>
2025-11-03 11:05:10 +1100

    Switch to Option class

--- ViewModels/MainWindowViewModel.cs
+++ ViewModels/MainWindowViewModel.cs
@@ -1,21 +1,24 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Linq;
 using System.Windows.Input;
-using Avalonia.Rendering.Composition.Animations;
 using CommunityToolkit.Mvvm.Input;
 
 namespace DataGridComboBoxTestApp1124.ViewModels
 {
     public partial class MainWindowViewModel : ViewModelBase
     {
+        public static Option Option1 = new Option(){Value = "Option 1"};
+        public static Option Option2 = new Option(){Value = "Option 2"};
+        public static Option AnotherOption2 = new Option() { Value = "Option 2" };
+        public static Option Option3 = new Option(){Value = "Option 3"};
+
         public List<RowItem> AllItems { get; } = new List<RowItem>
         {
 
-            new RowItem("Item 1", "Option 1", new ObservableCollection<string>(){"Option 1"}),
-            new RowItem("Item 2", "Option 2", new ObservableCollection<string>(){"Option 2"}),
-            new RowItem("Item 3", "Option 3", new ObservableCollection<string>(){"Option 3"}),
+            new RowItem("Item 1", Option1, new ObservableCollection<Option>(){Option1}),
+            new RowItem("Item 2", Option2, new ObservableCollection<Option>(){Option2}),
+            new RowItem("Item 3", Option3, new ObservableCollection<Option>(){AnotherOption2, Option3}),
         };
 
         public IEnumerable<RowItem> Items => AllItems
--- ViewModels/RowItem.cs
+++ ViewModels/RowItem.cs
@@ -18,27 +18,89 @@ namespace DataGridComboBoxTestApp1124.ViewModels
             set => SetProperty(ref _name, value);
         }
 
-        private string _selectedOption;
-        public string SelectedOption
+        private Option _selectedOption;
+        public Option SelectedOption
         {
             get => _selectedOption;
             set
             {
                 SetProperty(ref _selectedOption, value);
                 Debug.WriteLine($"SelectedOption for {Name} set to: {_selectedOption}");
-                SelectedOptionSetterHistory += (value ?? "null") + "; ";
+                SelectedOptionSetterHistory += (value?.Value ?? "null") + "; ";
+                OnPropertyChanged(nameof(SelectedOptionSetterHistory));
             }
         }
 
 
         public string SelectedOptionSetterHistory { get; set; } = string.Empty;
 
-        public ObservableCollection<string> Options { get; init; }
-        public RowItem(string name, string selectedOption, ObservableCollection<string> options)
+        public ObservableCollection<Option> Options { get; init; }
+
+        public RowItem(string name, Option selectedOption, ObservableCollection<Option> options)
         {
             _name = name;
             _selectedOption = selectedOption;
             Options = options;
+        }
+    }
+
+    public class Option : IEquatable<Option>
+    {
+        public string Value { get; set; }
+
+        public bool Equals(Option other)
+        {
+            if (ReferenceEquals(null, other))
+            {
+                return false;
+            }
+
+            if (ReferenceEquals(this, other))
+            {
+                return true;
+            }
+            return Value == other.Value;
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (ReferenceEquals(null, obj))
+            {
+                return false;
+            }
+
+            if (ReferenceEquals(this, obj))
+            {
+                return true;
+            }
+
+            if (obj.GetType() != typeof(Option))
+            {
+                return false;
+            }
+
+            return Equals((Option)obj);
+        }
+
+        public override int GetHashCode()
+        {
+            return Value?.GetHashCode() ?? 0;
+        }
+
+        /// <summary>
+        ///     Checks for equality.
+        /// </summary>
+        public static bool operator ==(Option left, Option right)
+        {
+            return Equals(left, right);
+        }
+
+        /// <summary>
+        ///     Checks for inequality.
+        /// </summary>
+        public static bool operator !=(Option left, Option right)
+        {
+            return !Equals(left, right);
         }
     }
 }
